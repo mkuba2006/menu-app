6b6823fbd791c3fcb040d01b905b6e8a4e2090b7
Jakub Myrta <mkuba2006@gmail.com>
2023-04-13 18:52:33 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,94 @@
+let foods = food;
+let product_container = document.getElementById('container');
+
+const b = document.querySelectorAll('button#nat');
+
+b.forEach(button =>{
+    button.addEventListener('click',()=>{
+        const value = button.value.toLowerCase();
+        button.classList.toggle('active');
+        if(button.classList.contains('active')){
+            const fitem = foods.filter((product)=>{
+                if(product.continent.toLowerCase().includes(value)){
+                    return product;
+                }
+            });
+            foods = fitem;
+            console.log(foods);
+            render(foods);
+        }else{
+            foods = food;
+            console.log(foods);
+            render(foods)
+        }
+    })
+})
+
+
+
+
+
+
+
+
+const a = document.querySelectorAll('button#type');
+a.forEach(grp =>{
+    grp.addEventListener('click',()=>{
+        const value = grp.value.toLowerCase();
+        grp.classList.toggle('active');
+        if(grp.classList.contains('active')){
+            const fgroup = foods.filter((product)=>{
+                if(product.group.toLowerCase().includes(value)){
+                    return product;
+                }
+            });
+            foods = fgroup;
+            console.log(foods);
+            render(foods);
+        }else{
+            foods = food;
+            console.log(foods);
+            render(foods)
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const render = (product) =>{
+    product_container.innerHTML='';
+    for(let i = 0; i < product.length; i++){
+        const new_prod = document.createElement('div');
+        new_prod.className= `product`;
+        new_prod.innerHTML =`
+        <div id="img_back"">
+            <img src="${product[i].img}"/>
+        </div>
+        <div id="content">
+            <div id="opis">
+                <h1 id='name'>${product[i].name}</h1>
+                <div id='continent'>${product[i].continent}</div>
+                <div id='country'>${product[i].country}</div>
+                <div id='group'>${product[i].group}</div>
+            </div>
+        </div>
+        `;
+        product_container.appendChild(new_prod);
+    }
+}
+
+document.onload = render(foods);
---
+++ food.js
@@ -0,0 +1,50 @@
+const food = [
+    {
+        id: 0,
+        continent:'europe',
+        country:'Italy',
+        group:'main courses',
+        name:'Pizza',
+        img: 'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/pizza-sycylijska.jpg'
+    },
+    {
+        id: 1,
+        continent:'europe',
+        country:'Turkey',
+        group:'Fast Food',
+        name:'Kebab',
+        img: 'https://www.pyszne.pl/foodwiki/uploads/sites/7/2018/03/kebab-2.jpg'
+    },
+    {
+        id: 2,
+        continent:'europe',
+        country:'Poland',
+        group:'soups',
+        name:'Żurek',
+        img: 'https://szefpoleca.pl/wp-content/uploads/2022/10/tradycyjny-zurek-na-zakwasie-900x575.webp'
+    },
+    {
+        id: 3,
+        continent:'europe',
+        country:'Italy',
+        group:'main course',
+        name:'Pizza',
+        img: 'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/pizza-sycylijska.jpg'
+    },
+    {
+        id: 4,
+        continent:'europe',
+        country:'Italy',
+        group:'main course',
+        name:'Pizza',
+        img: 'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/pizza-sycylijska.jpg'
+    },
+    {
+        id: 5,
+        continent:'North America',
+        country:'USA',
+        group:'Fast Food',
+        name:'Burger',
+        img: 'https://cdn.galleries.smcloud.net/t/galleries/gf-fCGi-H1PR-KYKb_amerykanski-burger-z-francuskim-sosem-jak-zrobic-664x442-nocrop.jpg'
+    }
+]
